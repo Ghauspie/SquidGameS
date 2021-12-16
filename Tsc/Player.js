a864ac28a6bc0c3934d8d4dfbb940b74650388e7
Ghauspie <hauspie.guillaume@free.fr>
2021-12-16 12:13:50 +0100

    page select player

--- Tsc/Player.js
+++ Tsc/Player.js
@@ -1,16 +1,14 @@
 "use strict";
-let Player1;
-let player2;
-let matricule1;
-let matricule2;
-let username1;
-let username2;
+let name;
+let matricule;
 function StartPlayer() {
     let selectPlayerDisplay;
     let index;
     index = document.getElementById('homePage');
-    index.setAttribute('class', 'hidden');
+    index.style.display = "none";
     selectPlayerDisplay = document.getElementById('selectPlayers');
     selectPlayerDisplay.removeAttribute('class', 'hidden');
     console.log("test start");
+}
+function SelectNumberPlayer() {
 }
--- Tsc/Player.ts
+++ Tsc/Player.ts
@@ -1,18 +1,17 @@
-let Player1: string;
-let player2: string;
-let matricule1:number;
-let matricule2:number;
-let username1:string|number;
-let username2:string|number;
+let name: string;
+let matricule:number;
 
 
 function StartPlayer(){
     let selectPlayerDisplay:any;
     let index:any;
     index=document.getElementById('homePage');
-    index.setAttribute('class','hidden');
+    index.style.display="none";
     selectPlayerDisplay=document.getElementById('selectPlayers');
     selectPlayerDisplay.removeAttribute('class','hidden');
-
     console.log("test start");
+}
+
+function SelectNumberPlayer(){
+
 }
