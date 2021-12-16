c2f7e12b5093476e0455e29fe0afe44a46f880e6
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2021-12-16 11:57:05 +0100

    class player hervé

--- Tsc/Player.js
+++ Tsc/Player.js
@@ -1,1 +1,31 @@
 "use strict";
+class player {
+    constructor(playerName, playerNumber) {
+        this.marbles = 10; //Chaque joueur a 10 billes au départ
+        this.name = playerName;
+        this.number = playerNumber;
+        this.marblesBet = 0;
+    }
+    getNumber() {
+        return "001";
+    }
+    ;
+    bet(marbles) {
+        this.marblesBet = marbles;
+    }
+    guess(pair, player2) {
+        this.marbles += player2.marblesBet;
+        return true;
+    }
+    isDead() {
+        return (this.marbles <= 0);
+    }
+}
+//Exemples d'utilisation
+// let p1 = new player("hervé", 1);
+// let p2 = new player("jc", 456);
+// console.log(p1.getNumber());
+// p1.bet(3);
+// if (p2.guess(true, p1)) {
+//     console.log("p2 a bien deviné un nombre pair et gagne" + p1.marbles);
+// };s
