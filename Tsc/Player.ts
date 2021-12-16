9c07c40330c1a2287ff12c3ed3e766056f9aa4bc
Hervé Go <gomanne.student@gmail.com>
2021-12-16 11:55:38 +0100

    Squashed commit of the following:
    commit 2e1b19c7aef4a170262cc21e120863c6e36ccc03
    Author: Hervé Go <gomanne.student@gmail.com>
    Date:   Thu Dec 16 11:54:51 2021 +0100
    
        go
    
    commit b2e7c7f5796a17303fe6809dfda75b4c7e358bf9
    Author: Hervé Go <gomanne.student@gmail.com>
    Date:   Thu Dec 16 11:47:46 2021 +0100
    
        Class Player "prototype"
    
    commit 69afb6b130180bb1b4be256fd302c83b8e9a70a6
    Merge: a7d7cee 1063b62
    Author: Hervé Go <gomanne.student@gmail.com>
    Date:   Thu Dec 16 10:00:05 2021 +0100
    
        Merge branch 'RV' of https://github.com/Ghauspie/SquidGameS into RV
    
    commit 1063b62fc2567838669870292503cf1ae7a94c13
    Merge: 12b5700 d43ca01
    Author: Hervé Go <gomanne.student@gmail.com>
    Date:   Thu Dec 16 09:59:26 2021 +0100
    
        Merge branch 'RV' of https://github.com/Ghauspie/SquidGameS into RV
    
    commit d43ca01dd3e46ccfbfb89f45ad818ead930b4c20
    Author: Hervé Go <gomanne.student@gmail.com>
    Date:   Thu Dec 16 09:22:01 2021 +0100
    
        Modif du titre

---
+++ Tsc/Player.ts
@@ -0,0 +1,41 @@
+class player {
+    marbles: number;
+    name: string;
+    private number: number;
+    private marblesBet: number;
+
+    constructor(playerName: string, playerNumber: number) {
+        this.marbles = 10; //Chaque joueur a 10 billes au départ
+        this.name = playerName;
+        this.number = playerNumber;
+        this.marblesBet = 0;
+    }
+
+    getNumber(): string {
+        return "001";
+    };
+
+    bet(marbles: number): void {
+        this.marblesBet = marbles;
+        
+    }
+
+    guess(pair: boolean, player2: player): boolean {
+        this.marbles += player2.marblesBet;
+        return true;
+    }
+
+    isDead() : boolean {
+        return (this.marbles <= 0);
+    }
+
+}
+
+//Exemples d'utilisation
+// let p1 = new player("hervé", 1);
+// let p2 = new player("jc", 456);
+// console.log(p1.getNumber());
+// p1.bet(3);
+// if (p2.guess(true, p1)) {
+//     console.log("p2 a bien deviné un nombre pair et gagne" + p1.marbles);
+// };s
