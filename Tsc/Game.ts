f251a177ea6c2cd7d291dddfff6161022f85abb4
Hervé Go <gomanne.student@gmail.com>
2021-12-17 16:07:11 +0100

    Billes cliquables pour pari
    La couleur des billes est stockée dans la classe player

--- Tsc/Game.ts
+++ Tsc/Game.ts
@@ -3,19 +3,37 @@ import {player} from "./Player";
 let p1 = new player("moi",123);
 let p2 = new player("lui",345)
 
-console.log("p2 mise 4 billes");
-p2.bet(4);
-console.log("p1 devine pair");
-if (p1.guess("even", p2)) {
-    console.log(`p1 a gagné, il a ${p1.marbles} billes, et p2 en a ${p2.marbles}.`);
-} else {
-    console.log("p1 a perdu ?");
-}
-console.log("p1 mise 3 billes");
-p1.bet(7);
-console.log("p2 devine impair");
-if (p2.guess("even", p1)) {
-    console.log(`p2 a gagné, il a ${p2.marbles} billes, et p1 en a ${p1.marbles}.`);
-} else {
-    console.log(`p2 a perdu, il a ${p2.marbles} billes, et p1 en a ${p1.marbles}.`);
+// console.log("p2 mise 4 billes");
+// p2.bet(4);
+// console.log("p1 devine pair");
+// if (p1.guess("even", p2)) {
+//     console.log(`p1 a gagné, il a ${p1.marbles} billes, et p2 en a ${p2.marbles}.`);
+// } else {
+//     console.log("p1 a perdu ?");
+// }
+// console.log("p1 mise 3 billes");
+// p1.bet(7);
+// console.log("p2 devine impair");
+// if (p2.guess("even", p1)) {
+//     console.log(`p2 a gagné, il a ${p2.marbles} billes, et p1 en a ${p1.marbles}.`);
+// } else {
+//     console.log(`p2 a perdu, il a ${p2.marbles} billes, et p1 en a ${p1.marbles}.`);
+// }
+let button = document.getElementById('testMarbles') as HTMLButtonElement; 
+button.addEventListener('click', addMarblesButtons);
+
+function addMarblesButtons() {
+    p1.marbles = Math.floor(Math.random()*19)+1;
+    console.log(`Ajout des ${p1.marbles} billes de ${p1.name}`);
+    let docContext = document.getElementById("btnMarbles") as HTMLDivElement;
+    docContext.innerHTML = "";
+    for( let i = 1; i <= p1.marbles; i++) {
+        let button = document.createElement("button") as HTMLButtonElement;
+        button.innerHTML = "<span class='big'> </span>" + i.toString();
+        button.id = "btnMarble" + i;
+        button.className = "marble marble" + p1.colorMarbles[i];
+        button.onclick = function() { p1.bet(i) };
+
+        docContext.appendChild(button);
+    }
 }
--- Tsc/Player.ts
+++ Tsc/Player.ts
@@ -4,6 +4,7 @@
 export class player {
     marbles: number;
     name: string;
+    colorMarbles: Array<number> = [];
     private matricule: number;
     private marblesBet: number;
 
@@ -17,6 +18,16 @@ export class player {
         this.name = playerName;
         this.matricule = matricule;
         this.marblesBet = 0;
+        this.initColors();
+    }
+
+    /**
+     * Attribue un numéro de couleur de bille aléatoire entre 1 et 9
+     */
+    private initColors(): void {
+        for( let i = 0; i < 20; i++) {
+            this.colorMarbles.push( Math.floor(Math.random()*9) + 1 );
+        }
     }
 
     /**
@@ -33,6 +44,7 @@ export class player {
      * @param marbles le nombre de billes misées
      */
     bet(marbles: number): void {
+        console.log(this.name + " bet " + marbles);
         if( marbles > this.marbles ) {
             throw new Error(`Le nombre de billes pariées (${marbles}) est supérieur au nombre de billes restantes (${this.marbles}).`);
         }
